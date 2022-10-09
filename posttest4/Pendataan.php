e0fb9c8c5229742289c9c3e4c8d98d186b88d516
Sherina Laraswati <93170060+SherinaLaraswati@users.noreply.github.com>
2022-10-09 22:01:09 +0800

    Add files via upload

---
+++ posttest4/Pendataan.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Pendataan Wear Time</title>
+        <link rel ="stylesheet"  href = "Pendataan.css">
+    </head>
+    <b>Pendataan Wear Time</b>
+    <form method="POST" action="hasil-pendataan.php">
+   
+        <div>
+            <label>Masukkan email anda: </label><br>
+            <input name = "email" type = "email" placeholder = "Klik disini...">
+        </div>
+        <div><br>
+            <label>Masukkan kata sandi: </label><br>
+            <input name ="password" type = "password" placeholder="Klik disini...">
+        </div>
+        <div><br>
+            <label>Masukkan nama anda: </label><br>
+            <input name="nama" type="text" placeholder="Klik disini...">
+        </div>
+        <div><br>
+            <label>Masukkan nomer telepon: </label><br>
+            <input name="nomer" type="number" placeholder="Klik disini..."> 
+        </div>
+
+        <div><br>
+            <label>Masukkan alamat anda: </label><br>
+            <input name="alamat" type="text" placeholder="Klik disini...">
+        </div>
+        <div><br>
+            <button>Submit</button>
+        </div>
+    </form>
+    <footer>
+        <div class = "outer-footer">
+        Copyright &copy; 2022 Desgin by SherinaLaraswati
+    </footer>
+
+</html>
